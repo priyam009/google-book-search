cfef95eee9b8fd31f76d911edba3d0fd3c91ba3a
Priyam <priyam009@live.com>
2019-10-30 00:35:41 +1100

    Add href references

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,15 +4,15 @@ import "../index.css";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="/">
         Google Books Search
       </a>
       <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <a className="nav-item nav-link active" href="#">
+          <a className="nav-item nav-link active" href="/">
             Search
           </a>
-          <a className="nav-item nav-link ml-5 mr-5" href="#">
+          <a className="nav-item nav-link ml-5 mr-5" href="/save">
             Save
           </a>
         </div>
