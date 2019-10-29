c8d57c251f290e64aebc04ab1bc68669db5a23a6
Priyam <priyam009@live.com>
2019-10-30 00:39:55 +1100

    Add search-content class name

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -3,7 +3,7 @@ import "../App.css";
 
 function Search(props) {
   return (
-    <div className="container">
+    <div className="container search-content">
       <h4>
         Book Search
       </h4>
