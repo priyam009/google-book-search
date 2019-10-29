b6c85cd0b540687dca49dd0a8946c16f94953e9b
Priyam <priyam009@live.com>
2019-10-29 23:42:29 +1100

    Add build

---
+++ client/build/precache-manifest.7dae2508d922bd7a60891e758b2c61cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5c2cbd6cbe8e9964d4b9",
+    "url": "/static/js/main.01693b84.chunk.js"
+  },
+  {
+    "revision": "66bbc66fb81735725762",
+    "url": "/static/js/2.4798774d.chunk.js"
+  },
+  {
+    "revision": "5c2cbd6cbe8e9964d4b9",
+    "url": "/static/css/main.2dda9460.chunk.css"
+  },
+  {
+    "revision": "fbee3dbfb797f2c8769c813f0066836d",
+    "url": "/index.html"
+  }
+];
