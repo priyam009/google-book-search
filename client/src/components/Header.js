fb9f119d9b29d4e01666076517936c5cf51405ac
Priyam <priyam009@live.com>
2019-10-30 00:17:23 +1100

    Update component to add style and content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "../App.css";
 
-function Header(props) {
-  return(
-    <div>
-      Google Books Search
+function Header() {
+  return (
+    <div class="jumbotron jumbotron-fluid text-center">
+      <div class="container">
+        <h1 class="display-4">Google Books Search</h1>
+        <p class="lead">Search for and Save Books of your Interest </p>
+      </div>
     </div>
   );
-};
+}
 
 export default Header;
