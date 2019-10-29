b9afdc29306425fc60c83fdcdaf7516abf45de0e
Priyam <priyam009@live.com>
2019-10-30 00:28:54 +1100

    Udpate class to className as per React conventions

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,10 +3,10 @@ import "../App.css";
 
 function Header() {
   return (
-    <div class="jumbotron jumbotron-fluid text-center">
-      <div class="container">
-        <h1 class="display-4">Google Books Search</h1>
-        <p class="lead">Search for and Save Books of your Interest </p>
+    <div className="jumbotron jumbotron-fluid text-center">
+      <div className="container">
+        <h1 className="display-4">Google Books Search</h1>
+        <p className="lead">Search for and Save Books of your Interest </p>
       </div>
     </div>
   );
