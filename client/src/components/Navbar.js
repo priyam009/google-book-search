5725d143110bd6481dc46c8a7ddf7be16a1c8108
Priyam <priyam009@live.com>
2019-10-30 00:30:06 +1100

    Update class to className as per React conventions

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,16 +3,16 @@ import "../index.css";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
         Google Books Search
       </a>
-      <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="#">
+      <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="#">
             Search
           </a>
-          <a class="nav-item nav-link ml-5 mr-5" href="#">
+          <a className="nav-item nav-link ml-5 mr-5" href="#">
             Save
           </a>
         </div>
