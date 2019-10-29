abed457bdee45098968c0f022db9ac9046ad9a1c
Priyam <priyam009@live.com>
2019-10-30 00:13:57 +1100

    Update to add header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
-      <Navbar />
+      <>
+        <Navbar />
+        <Header />
+      </>
     );
   }
 }
