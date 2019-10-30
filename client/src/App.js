2c40b678fde3d492f8ce7ca934d068b15820e607
Priyam <priyam009@live.com>
2019-10-30 22:08:26 +1100

    Add react-router-dom and update App to function

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
-import "./index.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Result from "./components/Result";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Navbar />
-        <Header />
-        <Search />
-        <Result />
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Search />
+      <Result />
+    </>
+  );
 }
 
 export default App;
