b1292120264fa517ef828626513880db039ab701
Priyam <priyam009@live.com>
2019-10-30 22:25:13 +1100

    Update component name to Form for search bar

---
+++ client/src/components/Form.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../index.css";
+
+function Search(props) {
+  return (
+    <div className="container search-content">
+      <h4>
+        Book Search
+      </h4>
+      <form>
+        <div className="form-group">
+          {/* <label htmlFor="inputBook">Book</label> */}
+          <input
+            type="email"
+            className="form-control"
+            id="inputBook"
+            aria-describedby="bookHelp"
+            placeholder="Enter Book"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary d-flex d-block justify-content-end">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Search;
