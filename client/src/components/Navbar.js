12ab06faba0f38a94069388ae8538e9632e9e49d
Priyam <priyam009@live.com>
2019-11-01 18:57:44 +1100

    Remove active from style

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
       </a>
       <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <a className="nav-item nav-link active" href="/">
+          <a className="nav-item nav-link" href="/">
             Search
           </a>
           <a className="nav-item nav-link ml-5 mr-5" href="/save">
