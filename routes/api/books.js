64017acf0f418829d844e63572868b02519a059f
Priyam <priyam009@live.com>
2019-11-01 19:01:35 +1100

    Add GET route

---
+++ routes/api/books.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  console.log("req", req);
+  // axios
+  //   .get("https://www.googleapis.com/books/v1/volumes?q", { params: req.query })
+  //   .then(({ data: { results } }) => res.json(results))
+  //   .catch(err => res.status(422).json(err));
+});
+
+module.exports = router;
