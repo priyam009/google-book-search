d4b93aa73735b4496d0532040e7711f49ac35196
Priyam <priyam009@live.com>
2019-11-01 18:57:06 +1100

    Update Form component to class and add onClick and onChange functions

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,29 +1,50 @@
 import React from "react";
 import "../index.css";
 
-function Search(props) {
-  return (
-    <div className="container search-content">
-      <h4>
-        Book Search
-      </h4>
-      <form>
-        <div className="form-group">
-          {/* <label htmlFor="inputBook">Book</label> */}
-          <input
-            type="email"
-            className="form-control"
-            id="inputBook"
-            aria-describedby="bookHelp"
-            placeholder="Enter Book"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary d-flex d-block justify-content-end">
-          Search
-        </button>
-      </form>
-    </div>
-  );
+class Form extends React.Component {
+
+  onInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.props.handleChange(name, value)
+  }
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.handleClick()
+  }
+
+  render() {
+
+    return (
+      <div className="container search-content">
+        <h4>Book Search</h4>
+        <form>
+          <div className="form-group">
+            {/* <label htmlFor="inputBook">Book</label> */}
+            <input
+              type="text"
+              name="title"
+              value={this.props.title}
+              onChange={this.onInputChange}
+              className="form-control"
+              id="inputBook"
+              aria-describedby="bookHelp"
+              placeholder="Enter Book"
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary d-flex d-block justify-content-end"
+            onClick={this.onFormSubmit}
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Search;
+export default Form;
