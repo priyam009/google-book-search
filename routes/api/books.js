f05bfcc176e7020625adacb49422b7f2c00ff7c0
Priyam <priyam009@live.com>
2019-11-01 20:55:30 +1100

    Add Google Books API GET request

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,11 +1,17 @@
+const axios = require("axios");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  console.log("req", req);
-  // axios
-  //   .get("https://www.googleapis.com/books/v1/volumes?q", { params: req.query })
-  //   .then(({ data: { results } }) => res.json(results))
-  //   .catch(err => res.status(422).json(err));
+  console.log("req", req.query);
+  const apiKey = "AIzaSyAOLhDUYDH4kz5qkq_n1zhO6QLvCsxe4DQ";
+  axios
+    .get(
+      "https://www.googleapis.com/books/v1/volumes?q=" + req.query + "&key=" + apiKey
+    )
+    // .get("https://www.googleapis.com/books/v1/volumes?q=game&key=AIzaSyBymZZ_4KpRi_3VkDn653yZiq-lW3Dq-w8")
+    .then(result => res.json(result.data))
+    .catch(err => res.status(422).json(err));
+
 });
 
 module.exports = router;
