52bfc3b3af2f5bfa35b8e773ab28fa390e789adf
Priyam <priyam009@live.com>
2019-11-01 20:56:00 +1100

    Update route path

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 
 //API routes
-router.use("/api/", apiRoutes);
+router.use("/api", apiRoutes);
 
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
