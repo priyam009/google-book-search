0c2fdef635bb82869a1b674e625fa29afb09aa63
Priyam <priyam009@live.com>
2019-11-02 20:51:13 +1100

    Add header-content class

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import "../index.css";
 
 function Header() {
   return (
-    <div className="jumbotron jumbotron-fluid text-center">
+    <div className="jumbotron jumbotron-fluid text-center header-content">
       <div className="container">
         <h1 className="display-4">Google Books Search</h1>
         <p className="lead">Search for and Save Books of your Interest </p>
