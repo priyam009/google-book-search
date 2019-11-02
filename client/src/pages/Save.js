e7d8202e7722a88670bc7cbd81aa78bd788e98c9
Priyam <priyam009@live.com>
2019-11-02 16:55:47 +1100

    Add state, Title and Book components to render

--- client/src/pages/Save.js
+++ client/src/pages/Save.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
 import "../index.css";
 import Header from "../components/Header";
-import Form from "../components/Form";
-import Result from "../components/Result";
+import Title from "../components/Title";
+import Books from "../components/Books";
+import API from "../utils/API";
 
 class Save extends Component {
+  state = {
+    books: {}
+  }
+
+  componentDidMount() {
+    API.getBooks()
+      .then(res => this.setState({ book: res.data }))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <>
       <Header />
-      <Form />
-      <Result title="Saved" action="Delete"/>
+      <Title heading="Saved">
+          {this.state.books.map((book, index) => (
+            <Books
+              key={index}
+              action="Save"
+              title={book.title}
+              subtitle={book.subtitle}
+              description={book.description}
+              image={book.image}
+              authors={book.authors}
+              link={book.link}
+              handleBook={this.handleBook}
+            />
+          ))}
+        </Title>
       </>
     )
   }
