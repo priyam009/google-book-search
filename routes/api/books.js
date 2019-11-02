2a0711e3d6b28401a59dd67bdf628e9e3ff7b64b
Priyam <priyam009@live.com>
2019-11-02 17:49:03 +1100

    Add routes to findall and delete books

--- routes/api/books.js
+++ routes/api/books.js
@@ -3,18 +3,27 @@ const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
 router.get("/google", (req, res) => {
-  console.log("req", req.query.q);
+  // console.log("req", req.query.q);
   const apiKey = "AIzaSyAOLhDUYDH4kz5qkq_n1zhO6QLvCsxe4DQ";
 
   axios
     .get(
-      "https://www.googleapis.com/books/v1/volumes?q=" + req.query.q + "&key=" + apiKey
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+        req.query.q +
+        "&key=" +
+        apiKey
     )
     .then(result => res.json(result.data))
     .catch(err => res.status(422).json(err));
-
 });
 
-router.route("/").post(booksController.create);
+// Matches with "/api/books"
+router
+  .route("/")
+  .get(booksController.findAll)
+  .post(booksController.create);
+
+// Matches with "/api/books/:id"
+router.route("/:id").delete(booksController.remove);
 
 module.exports = router;
