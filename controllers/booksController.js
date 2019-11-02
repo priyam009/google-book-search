8f14af4235ee132180040e3d49a9e2e2666055cc
Priyam <priyam009@live.com>
2019-11-02 17:48:29 +1100

    Add functionality to getAll and delete books

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,10 +2,20 @@ const db = require("../models");
 
 module.exports = {
   create: function(req, res) {
-    console.log("req.body", req.body);
-    db.Book
-      .create(req.body)
+    db.Book.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findAll: function(req, res) {
+    db.Book.find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Book.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
-}
+};
