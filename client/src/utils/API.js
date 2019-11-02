0870a3443b584af7af065d86e3948e0c356b4e05
Priyam <priyam009@live.com>
2019-11-02 17:47:46 +1100

    Get all books and Delete Book functionality added

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,7 +7,15 @@ export default {
   },
   // Saves a book to the database
   saveBook: function(bookData) {
-    console.log(bookData);
+    // console.log(bookData);
     return axios.post("/api/books", bookData);
+  },
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
   }
 };
