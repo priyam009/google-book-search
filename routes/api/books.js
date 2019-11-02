51dd48585d49286d91ba45a59aae39e1359a65bd
Priyam <priyam009@live.com>
2019-11-02 17:01:26 +1100

    Update Google API GET request, add POST request to save book and require booksController

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,17 +1,20 @@
 const axios = require("axios");
 const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
 
 router.get("/", (req, res) => {
-  console.log("req", req.query);
+  console.log("req", req.query.q);
   const apiKey = "AIzaSyAOLhDUYDH4kz5qkq_n1zhO6QLvCsxe4DQ";
+
   axios
     .get(
-      "https://www.googleapis.com/books/v1/volumes?q=" + req.query + "&key=" + apiKey
+      "https://www.googleapis.com/books/v1/volumes?q=" + req.query.q + "&key=" + apiKey
     )
-    // .get("https://www.googleapis.com/books/v1/volumes?q=game&key=AIzaSyBymZZ_4KpRi_3VkDn653yZiq-lW3Dq-w8")
     .then(result => res.json(result.data))
     .catch(err => res.status(422).json(err));
 
 });
+
+router.route("/").post(booksController.create);
 
 module.exports = router;
