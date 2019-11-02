935e08b7e0138b74b8c069dae5c0c06764c5527d
Priyam <priyam009@live.com>
2019-11-02 16:59:15 +1100

    Add axios post request to save book

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,5 +4,10 @@ export default {
   //Get all books from Google Books API
   getBooks: function(query) {
     return axios.get("/api/books/", { params: { q: query } });
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    console.log(bookData);
+    return axios.post("/api/books", bookData);
   }
 };
